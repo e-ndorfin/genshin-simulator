e5546b9e75e7711b675800388a64efe9d124629f
e-ndorfin <agarioloki@gmail.com>
2022-11-19 16:51:19 +0800

    final product

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from tkinter import *
-from tkvideo import tkvideo
+from PIL import Image 
 
-root = Tk()
-my_label = Label(root)
-my_label.pack()
-player = tkvideo("3star.mp4", my_label, loop = 1, size = (1280,720))
-player.play()
-
-root.mainloop()
+im = Image.open(r"/Users/zacharytang/desktop/coding/webDev/genshin_scam/img/cursor.png")
+ 
+# Size of the image in pixels (size of original image)
+# (This is not mandatory)
+width, height = im.size
+ 
+# Setting the points for cropped image
+im = im.resize((40,40), Image.LANCZOS)
+# Shows the image in image viewer
+im.show()
+im.save('/Users/zacharytang/desktop/coding/webDev/genshin_scam/img/cursor_resized.png', "PNG")
